8c591cf18c912f17531878f68151559b7882008c
Grant Kurtz <th3guys2@gmail.com>
2012-01-10 11:09:34 -0500

    The translator.c fill will turn assembly code into a 'binary executable'

---
+++ translator.c
@@ -0,0 +1,26 @@
+/**
+ * File:		translator.c
+ * Author:		Grant Kurtz
+ *
+ * Description:	Turns Hartz assembly code into a "binary executable".
+ */
+
+#include <sdtlib.h>
+#include <stdio.h>
+#include <cstring.h>
+
+// Machine Constraints
+#define MAX_REGS	2
+#define REG_ZERO	"$0"
+#define REG_ONE		"$1"
+#define MAX_MEMORY	28
+
+void main(int argc, char **argv){
+	
+	// perorm sanity check on arguments
+	
+	
+	// start processing file
+	
+	
+}
