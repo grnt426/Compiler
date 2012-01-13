a0ec4fead162e10c7c1853720aa3e7b22ec627a1
Grant Kurtz <th3guys2@gmail.com>
2012-01-13 17:50:22 -0500

    Updated some documentation, cleaned up unneeded DEBUG statements.

--- translator.c
+++ translator.c
@@ -55,6 +55,9 @@ int main(int argc, char **argv){
 	printf("\nDone.\n");
 }
 
+/**
+ * Opens a file for writing.
+ */
 FILE *open_write_file(const char *file){
 	FILE *out_file = 0;
 	out_file = fopen(file, "w");
@@ -65,6 +68,10 @@ FILE *open_write_file(const char *file){
 	return out_file;
 }
 
+/**
+ * Given a program struct, will process the (already opened) input file and
+ * begin compilation line by line.
+ */
 void process_input_program(struct program *program){
 	
 	printf("\nProcessing File...\n");
@@ -78,14 +85,10 @@ void process_input_program(struct program *program){
 			fprintf(stderr, "*** Reading Line %d...\n", program->line_count);
 		#endif
 		short ret = fgetpos(program->in, &program->str_line);
-		#ifdef DEBUG
-			fprintf(stderr, "LINE POS: '%p' RET VAL: '%d'\n", 
-					&(program->str_line), ret);
-		#endif
 		tok = read_next_token(buf, program->in, MAX_LINE_LEN);
 		
 		#ifdef DEBUG
-			fprintf(stderr, "Read Token '%s' '%d'\n", tok, strcmp(tok, "NOT"));
+			fprintf(stderr, "Read Token '%s' '%d'\n", tok);
 		#endif
 		
 		if(!tok){
@@ -124,6 +127,9 @@ void process_input_program(struct program *program){
 	#endif
 }
 
+/**
+ * Halt Instruction Processor.
+ */
 void process_halt(struct program *prog){
 	#ifdef DEBUG
 		fprintf(stderr, "HALT instruction read...\n");
@@ -131,6 +137,9 @@ void process_halt(struct program *prog){
 	write_str(HALT, prog->out);
 }
 
+/**
+ * Not Instruction Processor.
+ */
 void process_not(struct program *prog){
 	#ifdef DEBUG
 		fprintf(stderr, "NOT instruction read...\n");
@@ -140,6 +149,9 @@ void process_not(struct program *prog){
 	write_instruc_str(NOT, src, 0, dst, "", prog);
 }
 
+/**
+ * Checks for any unprocessed tokens that may have been left in the buffer.
+ */
 void check_garbage(struct program *prog){
 	#ifdef DEBUG
 		fprintf(stderr, "Checking for garbage at EOL...\n");
@@ -150,33 +162,53 @@ void check_garbage(struct program *prog){
 	}
 }
 
+/**
+ * Simply writes the given string to the given file in a standardized format.
+ */
 void write_str(char *str, FILE *out){
 	fputs(str, out);
 	fputs("\n", out);
 }
 
+/**
+ * Sets up a standard compiler error reporting message to indicate the file of
+ * failure and the line the failure occured on.
+ */
 void print_compiler_error(struct program *prog){
-	fprintf(stderr, "%s: %d:\n", prog->input, prog->line_count);
-	fsetpos(prog->in, &prog->str_line);
 	char buf[MAX_LINE_LEN+1];
 	buf[MAX_LINE_LEN] = 0;
+	fprintf(stderr, "%s: %d:\n", prog->input, prog->line_count);
+	
+	// Go back to start of line to show the user where the error occured.
+	fsetpos(prog->in, &prog->str_line);
 	fgets(buf, MAX_LINE_LEN, prog->in);
 	trimwhitespace(buf);
 	fprintf(stderr, "\t'%s'\n", buf);
 }
 
+/**
+ * Used for reporting that an unexpected token, where one may have not been 
+ * anticipated, that wasnot supposed to exist.
+ */
 void print_unexpected_ident(char *ident, struct program *prog){
 	print_compiler_error(prog);
 	fprintf(stderr, "\tUnexpected Identifier '%s'.\n", ident);
 	prog->error_code = GARBAGE;
 }
 
+/**
+ * Used for reporting instances where a token was being processed, but an 
+ * unexpected character was read where another was anticipated.
+ */
 void print_expected_ident(char *ident, char *expected, struct program *prog){
 	print_compiler_error(prog);
 	fprintf(stderr, "Expected '%s' but found '%s'.\n", expected, ident);
 	prog->error_code = GARBAGE;
 }
 
+/**
+ * A convenience function for finding the end of a given file.
+ */
 short check_EOF(FILE *file){
 	int c = fgetc(file);
 	if(feof(file))
@@ -185,6 +217,12 @@ short check_EOF(FILE *file){
 	return 0;
 }
 
+/**
+ * Will attempt to find a source register of the given format:
+ *		$S<x>
+ * Note, that x MUST be between 1 and MAX_REGS (inclusively). The token is 
+ * rejected if all 3 characters are not of the given format.
+ */
 short read_src_reg(struct program *prog){
 	char *tok = read_reg(prog);
 	short reg = 0;
@@ -195,10 +233,22 @@ short read_src_reg(struct program *prog){
 	return reg;
 }
 
+/**
+ * Like the 'read_src_reg()' function, will attempt to find a destination 
+ * register of the given format:
+ *		$D<x>
+ * Note, that x MUST be between 1 and MAX_REGS (inclusively). The token is
+ * rejected if all 3 characters are not of the given format.
+ */
 short read_dst_reg(struct program *prog){
 	return 1;
 }
 
+/**
+ * A general purpose function for ensuring the current token is of a valid 
+ * register type. Note, the token is rejected if the register is not exactly 3
+ * characters or does not start with a '$'.
+ */
 char *read_reg(struct program *prog){
 	char * tok = strtok(0, ", ");
 	
@@ -210,6 +260,11 @@ char *read_reg(struct program *prog){
 	return tok;
 }
 
+/**
+ * For more complex instructions that will come in pieces, this is useful as it 
+ * will print the "binary" data in the order of the parameters.  Note that misc
+ * is a "catch all" that can be used to print an arbitrary number of characters.
+ */
 void write_instruc_str(char *str, short s1, short s2, short dest, char *misc,
 		struct program *prog){
 	
@@ -232,6 +287,9 @@ void trimwhitespace(char *s){
 	memmove(s, p, l + 1);
 }
 
+/**
+ * A convenience function for turning an entire string into uppercase.
+ */
 void strtoupper(char *str, int len){
 	while(len){
 		str[len-1] = toupper(str[len-1]);
@@ -239,6 +297,11 @@ void strtoupper(char *str, int len){
 	}
 }
 
+/**
+ * Will process the first token of the given input buffer (or return the next
+ * token of the previously provided buffer if buf is 0), trim whitespace, 
+ * convert the entire buffer to uppercase, and then return the token.
+ */
 char *read_next_token(char *buf, FILE *input, int buf_size){
 	fgets(buf, buf_size, input);
 	trimwhitespace(buf);
