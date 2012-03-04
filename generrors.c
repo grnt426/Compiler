091e215c3346657426572dbc799e6ae229fa8b18
Grant Kurtz <th3guys2@gmail.com>
2012-03-04 11:09:15 -0500

    Added another function, print_fault, to deal with logical assertions that failed.  Will mostly be used to guard against nul-pointers, but canbe adapted.

--- generrors.c
+++ generrors.c
@@ -9,3 +9,9 @@ void print_memory_error(struct program *prog){
 	prog->error_code = ALLOC_ERR;
 }
 
+void print_fault(const char* reason, struct program *prog){
+	fprintf(stderr, "Whoops! The compiler has a bug! Failure Reason:\n"
+			"\t%s", reason);
+	prog->error_code = 4;
+}
+
