ac2d86de70518a5edf6af73696227fe2a788a125
Grant Kurtz <th3guys2@gmail.com>
2012-03-08 19:17:28 -0500

    Added documentation.

--- test.c
+++ test.c
@@ -61,24 +61,55 @@ int main(int argc, char **argv){
 	printf("Test Complete.\n");
 }
 
+/**
+ * Checks to make sure all relevant files exist.
+ *
+ * @param test_num		Which tests to check exist.
+ * @return				0 if the files exist and are readable, otherwise
+ * 						1;
+ */
 short check_files(int test_num){
 	return 0;
 }
 
+/**
+ * A convenience function for printing a colored asterisk in front of messages.
+ *
+ * @param	color	The color to make the asterisk.
+ * @param	indent	Any extra indentation (or anything really) to add before
+ * 					the asterisk.
+ * @param	out		Where to make the output go.
+ */
 void print_status(const char *color, const char *indent, FILE *out){
 	if(indent)
 		fprintf(out, "%s", indent);
 	fprintf(out, "%s * " RST_C, color);
 }
 
+/**
+ * Deletes older test result data so as not to confuse an older test result
+ * with a new one.
+ */
 void cleanup_older(){
-
+	
 }
 
+/**
+ * Makes sure the executable we are testing against exists and is executable.
+ *
+ * @return		0 on success, otherwise 1
+ */
 short check_executable(){
 	return 0;
 }
 
+/**
+ * Performs the test on the given exceutable and will wait until the test
+ * completes before returning.
+ *
+ * @param	exec		The program to test against
+ * @param	test_num	The numbered test to run against the executable.
+ */
 void run_test(char *exec, int test_num){
 	
 	int pid = 0, out, err;
@@ -88,6 +119,7 @@ void run_test(char *exec, int test_num){
 	
 	switch(pid){
 
+		// fork failed!
 		case -1:
 			print_status(RED_C, 0, stderr);
 			fprintf(stderr, "Unable to fork child!\n");
@@ -123,7 +155,7 @@ void run_test(char *exec, int test_num){
 			prog[2] = resbuf;
 			prog[3] = '\0'; // last argument must be nul
 			
-			// actually print
+			// actually execute the test
 			execvp(prog[0], prog);
 			print_status(RED_C, 0, stderr);
 			fprintf(stderr, "Unable to execute '%s'!\n", 
@@ -135,18 +167,31 @@ void run_test(char *exec, int test_num){
 	waitpid(pid, 0, 0);
 }
 
+/**
+ * Does a line-by-line comparison of the expected output against the actual
+ * output the program generated.
+ *
+ * @param	test_num	The numbered test that we will compare.
+ * @return				0 if the comparison found errors, otherwise 1.
+ */
 int compare_results(int test_num){
+
+	/*
 	char *outbuf = (char *) malloc(64);
 	memset(outbuf, 0, 64);
 	sprintf(outbuf, "%s%s%d.out", TEST_RES, TEST_FILE, test_num);
 	char *errbuf = (char *) malloc(64);
 	memset(errbuf, 0, 64);
 	sprintf(errbuf, "%s%s%d.err", TEST_RES, TEST_FILE, test_num);
+	*/
+
+	// Read from Example out
 	char *bbuf = (char *) malloc(64);
 	memset(bbuf, 0, 64);
-
 	sprintf(bbuf, "%s%s%d", TEST_OUT, TEST_FILE, test_num);
 	FILE *tres = fopen(bbuf, "r");
+
+	// Read from results of test
 	sprintf(bbuf, "%s%s%d.b", TEST_RES, TEST_FILE, test_num);
 	FILE *res = fopen(bbuf, "r");
 	if(!res){
@@ -156,11 +201,14 @@ int compare_results(int test_num){
 		return 1;
 	}
 
+	// setup some buffers for comparison
 	char *tline, *oline;
 	tline = (char *) malloc(64);
 	oline = (char *) malloc(64);
 	int line = 0;
 	int failure = 0;
+
+	// check each line
 	while(fgets(tline, 64, tres)){
 		fgets(oline, 64, res);
 		
@@ -193,6 +241,7 @@ int compare_results(int test_num){
 		line++;
 	}
 
+	// summary of this test
 	if(failure)
 		print_test_failed();
 	else
@@ -200,11 +249,17 @@ int compare_results(int test_num){
 	return failure;
 }
 
+/**
+ * A convenience function for indicating the test failed.
+ */
 void print_test_failed(){
 	print_status(RED_C, 0, stdout);
 	printf("Test Failed!\n");
 }
 
+/**
+ * A convenience function for indicating a test was successful.
+ */
 void print_test_success(){
 	print_status(GRN_C, 0, stdout);
 	fprintf(stdout, "Test Successful!\n");
