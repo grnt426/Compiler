467eedb88e327e0be79060ef44826f56e37adf05
Grant Kurtz <th3guys2@gmail.com>
2012-03-10 01:14:09 -0500

    Had to update terms to allow for better error reporting.

--- terms.h
+++ terms.h
@@ -3,14 +3,16 @@
 
 /**
  * Term
- * char* term	Contains the string that represents this term
- * int pos		The position of this term relative to the start term
+ * char* term			Contains the string that represents this term
+ * int pos				The position of this term relative to the start term
+ * int absolute_pos		The absolute position of the term in the file
  * struct Term **	The direct children of this term
  * struct Term *	The term that follows this term
  */
 struct Term{
 	char* term;
 	int pos;
+	int absolute_pos;
 	int child_count;
 	short trans;
 	struct Term **child_terms;
@@ -20,8 +22,7 @@ struct Term{
 struct program;
 
 // Term Manipulation Functions
-void 			add_child_term(struct Term *, struct Term *, 
-		struct program *prog);
+void 	add_child_term(struct Term *c, struct Term *t, struct program *prog);
 struct Term* 	create_term(char* term, unsigned int term_len, int children);
 struct Term* 	create_single_char_term(const char term, int children);
 
